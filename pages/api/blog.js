9fea3b1958eb4dc26a7fe926988293d68094514a
cy-bug <shashwat5590@gmail.com>
2023-03-08 01:46:49 +0530

    removing the use of environment variables

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   const blogId = req.query.id;
-  const url = `https://raw.githubusercontent.com/${process.env.GITHUB_USERNAME}/blogs/${process.env.GITHUB_BRANCH}/${blogId}.md`;
+  const url = `https://raw.githubusercontent.com/shashwtt/blogs/main/${blogId}.md`;
 
   try {
     const response = await axios.get(url);
--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default async function handler(req, res) {
   try {
-    const response = await axios.get(`https://raw.githubusercontent.com/${process.env.GITHUB_USERNAME}/blogs/${process.env.GITHUB_BRANCH}/data.json`)
+    const response = await axios.get(`https://raw.githubusercontent.com/shashwtt/blogs/main/data.json`)
     const data = response.data
     res.status(200).json(data)
   } catch (error) {
